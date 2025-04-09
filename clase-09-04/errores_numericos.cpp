3d9bc4cbbb2fb414a82a444457288f141821b8ba
jgutierrezco <jgutierrezco@unal.edu.co>
2025-04-09 11:43:06 -0500

    subío (_8(1)

---
+++ clase-09-04/errores_numericos.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+
+int main(void)
+{
+    int a = 1;
+
+    for(int a = 1; a > 0 ; a*=2){
+        std::cout << a << std::endl;
+    }
+
+
+    
+    
+    return 0;
+}
